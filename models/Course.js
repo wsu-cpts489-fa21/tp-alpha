22880feb52db5a24549acb2c1079065d575c78ae
Brian Schimert <brian.schimert@gmail.com>
2021-12-01 14:51:57 -0700

    update Course model

--- models/Course.js
+++ models/Course.js
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 
 const CourseSchema = new mongoose.Schema({
-    date: {type: Date, required: true},
-    course: {type: String, required: true},
-    type: {type: String, required: true, enum: ['practice','tournament']},
-    holes: {type: Number, required: true, min: 1, max: 18},
-    strokes: {type: Number, required: true, min: 1, max: 300},
-    minutes: {type: Number, required: true, min: 1, max: 240},
-    seconds: {type: Number, required: true, min: 0, max: 60},
-    notes: {type: String}
+    name: {type: String, required: true},
+    address: {type: String, required: true},
+    phoneNumber: {type: String, required: false},
+    location: {type: String, required: false},
+    picture: {type: String, required: false}
   },
   {
     toObject: {
