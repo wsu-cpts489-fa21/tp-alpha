639224f86d2910b8d38f3e2e68ea663d92302c52
d5k1y02 <d5k1y02@gmail.com>
2021-12-07 19:33:20 -0800

    added search function to course table -NOT TESTED

--- client/src/components/CourseTable.js
+++ client/src/components/CourseTable.js
@@ -8,6 +8,29 @@ class CourseTable extends React.Component {
         return table;
     }
 
+    searchCourseTable(searchVal) {
+        searchVal = searchVal.toUpperCase(); //case insensitive
+        let tr = table.getElementsByTagName("tr");
+        let td, rowText, i, j;
+        let numVisibleRows = 0;
+        for (i = 1; i < tr.length; i++) {  //Loop through all table rows
+          td = tr[i].getElementsByTagName("td");
+          rowText = "";
+            rowText += td[0].textContent; //only consider Course name Column
+          if (rowText.toUpperCase().indexOf(searchVal) > -1) {
+            tr[i].style.display = ""; //show row
+            numVisibleRows++;
+          } else {
+            tr[i].style.display = "none"; //hide row
+          }
+        }
+        if (numVisibleRows == 1) {
+          roundsTableCaption.textContent = "Table displaying 1 speedgolf course";
+        } else {
+          roundsTableCaption.textContent = "Table displaying " + numVisibleRows + " speedgolf courses";
+        }
+      }
+
     render() {
         return (
             <div id="courseModeTab" className="mode-page" role="tabpanel"
