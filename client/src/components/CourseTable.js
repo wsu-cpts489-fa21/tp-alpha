78bf74f239816f18d2f2788fd15193dabcd3bb14
d5k1y02 <d5k1y02@gmail.com>
2021-12-08 13:53:08 -0800

    added react autocomplete comenent to search bar

--- client/src/components/CourseTable.js
+++ client/src/components/CourseTable.js
@@ -17,29 +17,6 @@ class CourseTable extends React.Component {
         return table;
     }
 
-    /* searchCourseTable(searchVal) {
-        searchVal = searchVal.toUpperCase(); //case insensitive
-        let tr = table.getElementsByTagName("tr");
-        let td, rowText, i;
-        let numVisibleRows = 0;
-        for (i = 1; i < tr.length; i++) {  //Loop through all table rows
-          td = tr[i].getElementsByTagName("td");
-          rowText = "";
-            rowText += td[0].textContent; //only consider Course Name column
-          if (rowText.toUpperCase().indexOf(searchVal) > -1) {
-            tr[i].style.display = ""; //show row
-            numVisibleRows++;
-          } else {
-            tr[i].style.display = "none"; //hide row
-          }
-        }
-        if (numVisibleRows == 1) {
-          courseTableCaption.textContent = "Table displaying 1 speedgolf course";
-        } else {
-          courseTableCaption.textContent = "Table displaying " + numVisibleRows + " speedgolf courses";
-        }
-      } */
-
     render() {
         return (
             <div id="courseModeTab" className="mode-page" role="tabpanel"
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 class SearchBar extends React.Component {
+
+  handleOnSearch = (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  handleOnHover = (result) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  handleOnSelect = (item) => {
+    // the item selected
+    console.log(item)
+  }
+
+  handleOnFocus = () => {
+    console.log('Focused')
+  }
+
+  formatResult = (item) => {
+    return item
+    // return (<p dangerouslySetInnerHTML={{__html: '<strong>'+item+'</strong>'}}></p>); //To format result as html
+  }
     
     render() {
        return (
-        <input type="text" id="courseSearch" placeholder="Search for course.."></input>
+        <ReactSearchAutocomplete
+            items={this.props.courses}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
+          />
     ); 
   }
 }
