9d3cbc73844f3876060d8f773ed82ba5d3e55b9b
Brian Schimert <brian.schimert@gmail.com>
2021-12-15 13:18:33 -0700

    add tees to Course model

--- models/Course.js
+++ models/Course.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
+import {TeeSchema} from './Tee.js';
 
 const CourseSchema = new mongoose.Schema({
     name: {type: String, required: true},
     address: {type: String, required: true},
     phoneNumber: {type: String, required: false},
     location: {type: String, required: false},
-    picture: {type: String, required: false}
+    picture: {type: String, required: false},
+    tees:[TeeSchema]
   },
   {
     toObject: {
